00d009fdb4b7616248346017f03805717bcae90b
Peter Bell <peter@pbell.com>
2011-01-28 18:10:57 -0500

    Upgrade to latest rspec and autotest (couldn't find a reference to pull down spec or spec/autorun when I tried to rvm <gemset> gem install them, so had to do this to be able to run the test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'simple_geo'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'autotest'
 require 'fakeweb'
 
 Spec::Runner.configure do |config|
